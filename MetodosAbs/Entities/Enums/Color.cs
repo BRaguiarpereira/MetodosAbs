5f8b266c7335e32dd6e5c49b87ce0b810359f1ea
Brian aguiar <briantbini@outlook.com>
2021-05-03 14:05:48 -0300

    Escopo criado sem interação com usuario (sem main)

---
+++ MetodosAbs/Entities/Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodosAbs.Entities.Enums
+{
+    enum  Color 
+    {
+        BLACK,
+        BLUE,
+        RED
+    }
+}
---
+++ MetodosAbs/Entities/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MetodosAbs.Entities.Enums;
+
+namespace MetodosAbs.Entities
+{
+    // Classe abstract 
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+        // Metodo abstract 
+        public abstract double Area();
+
+    }
+
+}
